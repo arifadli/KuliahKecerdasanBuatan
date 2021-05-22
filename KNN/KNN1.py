f0012a9c80af72b66827024edb8dfe2675b5e7c5
arifadli <fadli.te.unsoed@gmail.com>
2021-05-22 20:57:57 +0700

    Upload Kode python

---
+++ KNN/KNN1.py
@@ -0,0 +1,88 @@
+matplotlib notebook
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import neighbors
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import matplotlib.patches as mpatches
+
+data = pd.read_table('fruit_data.txt')  # Pastikan file data set berada dalam folder yang sama dengan file jupyter notebook
+print(data.shape)
+data.head(10) # Menampilkan 10 baris pertama dari tabel
+
+## membuat dict key pair value  antara fruit_label dengan fruit_name
+lookup_fruit_name = dict(zip(data.fruit_label.unique(), data.fruit_name.unique()))   
+print(lookup_fruit_name)
+
+# Pada kasus ini explanatory variablenya adalah mass, width dan height sedangkan target variablenya adalah fruit_label
+X = data[['mass', 'width', 'height']]
+y = data['fruit_label']
+
+
+# kita akan mensplit menjadi 80% train data and 20% test data 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+# Chek dimensi array dari massing masing variable
+print('X_train = ', X_train.shape)
+print('X_test = ', X_test.shape)
+print('y_train = ', y_train.shape)
+print('y_test = ', y_test.shape)
+
+X_train.head()
+y_train.head()
+
+# Create classifier objet
+knn = KNeighborsClassifier(n_neighbors = 5)
+
+# Train the classifier using train data
+knn.fit(X_train, y_train)
+
+knn.score(X_test, y_test)
+
+def plot_fruit_knn(X, y, n_neighbors, weights):
+    X_mat = X[['height', 'width']].as_matrix()
+    y_mat = y.as_matrix()
+
+    # Create color maps
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF','#AFAFAF'])
+    cmap_bold  = ListedColormap(['#FF0000', '#00FF00', '#0000FF','#AFAFAF'])
+
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X_mat, y_mat)
+
+    # Plot the decision boundary by assigning a color in the color map
+    # to each mesh point.
+    
+    mesh_step_size = .01  # step size in the mesh
+    plot_symbol_size = 50
+    
+    x_min, x_max = X_mat[:, 0].min() - 1, X_mat[:, 0].max() + 1
+    y_min, y_max = X_mat[:, 1].min() - 1, X_mat[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, mesh_step_size),
+                         np.arange(y_min, y_max, mesh_step_size))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    # Plot training points
+    plt.scatter(X_mat[:, 0], X_mat[:, 1], s=plot_symbol_size, c=y, cmap=cmap_bold, edgecolor = 'black')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    patch0 = mpatches.Patch(color='#FF0000', label='apple')
+    patch1 = mpatches.Patch(color='#00FF00', label='mandarin')
+    patch2 = mpatches.Patch(color='#0000FF', label='orange')
+    patch3 = mpatches.Patch(color='#AFAFAF', label='lemon')
+    plt.legend(handles=[patch0, patch1, patch2, patch3])
+
+        
+    plt.xlabel('height (cm)')
+    plt.ylabel('width (cm)')
+    
+    plt.show()
+    
